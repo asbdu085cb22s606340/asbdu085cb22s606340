b5a66455b6bb06bfbcc97665b0aac60952413d6d
asbdu085cb22s606340 <144428776+asbdu085cb22s606340@users.noreply.github.com>
2023-10-10 20:07:40 +0530

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,11 @@
+Class Player:
+  def __init__(self,name,position,jersey_number):
+    self.name=name
+    self.position=position
+    self.jersey_number=jersey_number
+  def introduce(self):
+    return f"{self.name},I play as aself.position},and my jersey number is {self.jersey_numbr}."
+class Team:
+  if __name__"__main__":
+  player1=palyer("John Doe","forward",10)
+  print(player1.introduce())
